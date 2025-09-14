a6c2fe8af8d7e8eaa7796925130c770f81fc7b2d
shahram <shahramsamar2010@gmail.com>
2025-09-15 00:21:24 +0330

    Create f-string.py

---
+++ f-string.py
@@ -0,0 +1,6 @@
+name = input("enter name: ")
+id = input("enter id: ")
+gpa = float(input("enter gpa: "))
+
+print(f"name:{name}, id: {id}, gpa:{gpa}")
+print(f"name:{name}, id: {id}, gpa:{gpa: .2f}")
